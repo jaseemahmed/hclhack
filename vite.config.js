4dc9c5a455b5eae5cc6cb499612f7f574f5bc17e
jaiatearth <jayashree.sakthivel@gmail.com>
2025-06-28 13:30:21 +0400

    Jay-7 : Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react({
+      jsxRuntime: 'automatic', // ✅ Enables React 17+ JSX (no need to import React)
+    })
   ],
 })
